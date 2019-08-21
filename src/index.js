311f90b677319acf5c8b33352d87b005c0651a8c
Samantha  Nunez <samanthan-c@herbalife.com>
2019-08-21 15:28:30 -0500

    Adding theme and global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import theme from "./theme";
+import GlobalStyle from "./global-style";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+    <GlobalStyle />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  font: {
+    s: "8px",
+    m: "12px",
+    l: "14px"
+  },
+  colors: {
+    orange: "#cc7537",
+    grey: "#383634"
+  }
+};
+
+export default theme;
