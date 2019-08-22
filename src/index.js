46cc667414cdf47d897ffad973d20fcdb041f077
Samantha  Nunez <samanthan-c@herbalife.com>
2019-08-22 18:15:32 -0500

    Adding line breaks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { ThemeProvider } from "styled-components";
+
 import App from "./App";
 import GlobalStyle from "./global-style";
 import theme from "./theme";
