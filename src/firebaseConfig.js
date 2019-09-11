a19506e6427ea0d5174380f68d8d65ac3e717ba4
Samantha  Nunez <samanthan-c@herbalife.com>
2019-09-10 20:28:07 -0500

    Creating login

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,7 +4,7 @@ const {
   REACT_APP_DATABASE_URL,
   REACT_APP_PROJECT_ID,
   REACT_APP_MESSAGING_SENDER_ID,
-  REACT_APP_APP_ID,
+  REACT_APP_APP_ID
 } = process.env;
 
 const firebaseConfig = {
@@ -12,10 +12,9 @@ const firebaseConfig = {
   authDomain: REACT_APP_AUTH_DOMAIN,
   databaseURL: REACT_APP_DATABASE_URL,
   projectId: REACT_APP_PROJECT_ID,
-  storageBucket: "",
+  storageBucket: '',
   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
+  appId: REACT_APP_APP_ID
 };
-
 
 export default firebaseConfig;
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
-const Login = () => {
-  return <div>Login</div>;
+import toast from 'toast';
+
+import { Button, Container, Loader } from '../../components';
+import Card from './style';
+import logo from '../../../src/static/logo.png';
+import theme from '../../theme';
+
+const Login = ({ setIsAuthenticated }) => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const authentication = async (email, password) => {
+    setLoading(true);
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+
+      toast.success('Welcome');
+      setIsAuthenticated(true);
+      setLoading(false);
+    } catch (error) {
+      const errorMessage = error.message;
+
+      toast.danger(errorMessage);
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = e => {
+    const { email, password } = data;
+
+    e.preventDefault();
+    authentication(email, password);
+  };
+
+  const handleChange = e => {
+    const {
+      target: { name, value }
+    } = e;
+
+    setData({ ...data, [name]: value });
+  };
+
+  return (
+    <Container>
+      <Card theme={theme}>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="image-container">
+            <img src={logo} alt="logo" width="130" height="45" />
+          </div>
+          <div className="title">Log in</div>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={data.email || ''}
+            required
+            onChange={handleChange}
+            placeholder="Email"
+          />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={data.password || ''}
+            required
+            onChange={handleChange}
+            placeholder="Password"
+          />
+          <div className="button-container">
+            <div className="some-text">
+              Don't have an account? <Link to="/register">Register</Link>
+            </div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <Button type="submit" theme={theme}>
+                Log in
+              </Button>
+            )}
+          </div>
+        </form>
+      </Card>
+    </Container>
+  );
 };
 
 export default Login;
