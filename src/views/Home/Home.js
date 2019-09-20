10ebbf3d6cd2ac4ecf19bd712c241cf4df76657d
Samantha  Nunez <samanthan-c@herbalife.com>
2019-09-20 18:07:23 -0500

    Adjust something pick ae89c85 Feature, forgot password

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import BaselineAccountCircle from 'react-md-icon/dist/BaselineAccountCircle';
 
-import { getStories, getStory } from './api';
 import { Container, Navbar, Content } from './style';
 import logo from '../../static/logo-white.png';
 import CardStorie from './components/card';
 import Loader from '../../components/Loader';
+import { getStories, getStory } from './api';
 
 const Home = () => {
   const [stories, setStories] = useState([]);
